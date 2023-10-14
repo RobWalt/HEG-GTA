2a0aa88a3e0217989fced13f63bb1bdc400bed28
RobWalt <robwalter96@gmail.com>
2023-10-14 13:59:19 +0200

    chore: add Cargo.lock file

--- nannou-new.nix
+++ nannou-new.nix
@@ -1,26 +1,30 @@
 { pkgs, ... }:
 pkgs.rustPlatform.buildRustPackage rec {
   pname = "nannou_new";
-  version = "0.18.0";
+  version = "0.15.0";
 
   src = pkgs.fetchFromGitHub {
     owner = "nannou-org";
     repo = "nannou";
     rev = "v${version}";
-    hash = "";
+    hash = "sha256-5uiHluuCdDNbpzKB/nG/Rmp0u3D6269CLxv1Yv69VqQ=";
+  };
+
+  cargoLock = {
+    lockFile = ./nannou-cargo.lock;
   };
 
   sourceRoot = "${src.name}/nannou_new";
 
   cargoHash = "";
-  nativeBuildInputs = [ ];
 
+  nativeBuildInputs = [ ];
   buildInputs = [ ];
 
   meta = with pkgs.lib; {
     description = "A simple interactive tool for generating projects.";
     homepage = "https://github.com/nannou-org/nannou";
-    license = with pkgs.licenses; [ mit /* or */ apache ];
-    maintainers = with pkgs.maintainers; [ robwalt ];
+    license = with licenses; [ mit /* or */ asl20 ];
+    maintainers = with maintainers; [ robwalt ];
   };
 }
