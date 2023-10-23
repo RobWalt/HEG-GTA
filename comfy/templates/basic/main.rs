ceee1c5cb7814da91a237dfd66f9c1ea2ed0343a
aviac <aviac@mailbox.org>
2023-10-23 12:43:02 +0200

    chore: remove comments and name not fitting

--- comfy/templates/basic/main.rs
+++ comfy/templates/basic/main.rs
@@ -1,10 +1,7 @@
 use comfy::*;
 
-// This example shows an integration between comfy and blobs, a simple 2d physics engine. It's not
-// the most beautiful example, and maybe a bit verbose for what it does, but it tries to showcase
-// some more extensible ways of using comfy.
 comfy_game!(
-    "Physics Example",
+    "Basic Game",
     GameContext,
     GameState,
     make_context,
