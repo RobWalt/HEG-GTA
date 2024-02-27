a5905cc61c61e0d88a8803faee84f5657da5dff8
RobWalt <robwalter96@gmail.com>
2024-02-27 18:28:21 +0100

    feat(godot): start godot automaticall
    Authored-by: RobWalt <robwalter96@gmail.com>

--- nix/shells/godot.nix
+++ nix/shells/godot.nix
@@ -1,7 +1,11 @@
 {
   perSystem = { pkgs, ... }: {
     devShells.godot = pkgs.mkShell rec {
-      buildInputs = [ pkgs.godot_4 ];
+      packages = [ pkgs.godot_4 ];
+      shellHook = "
+      godot4
+      exit
+      ";
     };
   };
 }
